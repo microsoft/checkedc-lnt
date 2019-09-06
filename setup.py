889a2f15a7420525b71a38943e86b91ad0f16b22
Mandeep Singh Grang <magrang@microsoft.com>
2019-09-06 10:24:14 -0700

    Remove extra import

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ try:
 except ImportError:
     # The req module has been moved to pip._internal in the 10 release.
     from pip._internal.req import parse_requirements
-from pip.req import parse_requirements
 import lnt
 import os
 from sys import platform as _platform
